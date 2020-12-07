3d945a8e8922cfe0cd51b3d5c151827afc6d9e3c
mpodchul <53252009+mpodchul@users.noreply.github.com>
2020-12-07 20:55:51 +0100

    refactor and UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,54 +10,32 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$noteNumber.wav');
   }
 
+  Expanded buildKey(int number, Color color) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playNote(number);
+        },
+        color: color,
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              FlatButton(
-                onPressed: () {
-                  playNote(1);
-                },
-                color: Colors.red,
-              ),
-              FlatButton(
-                onPressed: () {
-                  playNote(2);
-                },
-                color: Colors.orange,
-              ),
-              FlatButton(
-                onPressed: () {
-                  playNote(3);
-                },
-                color: Colors.yellow,
-              ),
-              FlatButton(
-                onPressed: () {
-                  playNote(4);
-                },
-                color: Colors.green,
-              ),
-              FlatButton(
-                onPressed: () {
-                  playNote(5);
-                },
-                color: Colors.blueGrey,
-              ),
-              FlatButton(
-                onPressed: () {
-                  playNote(6);
-                },
-                color: Colors.blue,
-              ),
-              FlatButton(
-                onPressed: () {
-                  playNote(7);
-                },
-                color: Colors.purple,
-              ),
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.amber),
+              buildKey(3, Colors.green),
+              buildKey(4, Colors.pink),
+              buildKey(5, Colors.blue),
+              buildKey(6, Colors.orange),
+              buildKey(7, Colors.purple),
             ],
           ),
         ),
