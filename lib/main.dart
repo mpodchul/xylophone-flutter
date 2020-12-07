be4d5a488175891e3cf62390b0220de390b6c576
mpodchul <53252009+mpodchul@users.noreply.github.com>
2020-12-07 20:39:20 +0100

    audio playback and simple ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   @override
+  void playNote(int noteNumber) {
+    final player = AudioCache();
+    player.play('note$noteNumber.wav');
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              FlatButton(
+                onPressed: () {
+                  playNote(1);
+                },
+                color: Colors.red,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playNote(2);
+                },
+                color: Colors.orange,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playNote(3);
+                },
+                color: Colors.yellow,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playNote(4);
+                },
+                color: Colors.green,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playNote(5);
+                },
+                color: Colors.blueGrey,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playNote(6);
+                },
+                color: Colors.blue,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playNote(7);
+                },
+                color: Colors.purple,
+              ),
+            ],
+          ),
         ),
       ),
     );
